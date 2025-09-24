e2999c90b9255c8ead914abd5461f80c81a16fe2
LANCECRUZ-STUDENT <G00428876@atu.ie>
2025-09-24 14:47:34 +0100

    Added Controller to project

--- src/main/java/com/example/week2v2/Week2Application.java
+++ src/main/java/com/example/week2v2/Week2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Week2v2Application {
+public class Week2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Week2v2Application.class, args);
+        SpringApplication.run(Week2Application.class, args);
     }
 
 }
