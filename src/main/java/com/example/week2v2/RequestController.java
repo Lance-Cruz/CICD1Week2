545734480becdce00551b07f38d4d3314ddcb9cf
LANCECRUZ-STUDENT <G00428876@atu.ie>
2025-09-24 15:03:26 +0100

    Added PathVariable method to display inputted String

--- src/main/java/com/example/week2v2/RequestController.java
+++ src/main/java/com/example/week2v2/RequestController.java
@@ -1,6 +1,7 @@
 package com.example.week2v2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class RequestController {
     public String hello()
     {
         return "Hello";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name){
+        return "Hello " + name + "!";
     }
 }
