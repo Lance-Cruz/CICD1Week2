bd17687c91935c23b51c2715ecf217e4648b2e45
LANCECRUZ-STUDENT <G00428876@atu.ie>
2025-09-24 14:37:07 +0100

    Started project

---
+++ src/main/java/com/example/week2v2/Week2v2Application.java
@@ -0,0 +1,14 @@
+package com.example.week2v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2v2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week2v2Application.class, args);
+    }
+    //test
+
+}
---
+++ src/test/java/com/example/week2v2/Week2v2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week2v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2v2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
